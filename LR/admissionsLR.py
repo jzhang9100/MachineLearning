ded113844d8b745c7b185e678ccac6ab33bde1fb
cxz416 <cxz416@case.edu>
2020-08-07 20:35:31 -0500

    data loader for graduation

--- LR/admissionsLR.py
+++ LR/admissionsLR.py
@@ -1,7 +1,21 @@
 #!/bin/usr/python3
 from dataLoader import data_loader
+import numpy as np
 
 d = data_loader(0.8)
 d.graduation()
 (x_train, y_train), (x_test, y_test) = d.load_data()
+m = len(x_train)
+print("Training Data Size: " + str(m))
 
+#establish weights - 0 and baises b
+w = np.ones(x_train.shape[0])
+b = np.zeros(x_train.shape[0])
+
+
+def h(x,w,b):
+    #tanspose to get [9 x 400] * [400]i
+    return x+b
+
+t = h(x_train,w, b)
+print(t.shape, t)
