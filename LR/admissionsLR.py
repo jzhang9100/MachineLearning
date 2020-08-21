344e7e719df77467f0f8a880c4b50b64c758a1d8
cxz416 <cxz416@case.edu>
2020-08-21 03:32:06 -0500

    update admissions lr png

--- LR/admissionsLR.py
+++ LR/admissionsLR.py
@@ -15,10 +15,17 @@ for i, feature in enumerate(xtrain_T):
     std = np.std(feature)
     feature = (feature-mean)/std
     xtrain_T[i] = feature
-
 x_train = np.transpose(xtrain_T)
 
-lr = LogisticRegression(x_train, y_train, x_test, y_test, 200, 0.005)
+xtest_T = np.transpose(x_test)
+for i, feature in enumerate(xtest_T):
+    mean = np.mean(feature)
+    std = np.std(feature)
+    feature = (feature-mean)/std
+    xtest_T[i] = feature
+x_test = np.transpose(xtest_T)
+
+lr = LogisticRegression(x_train, y_train, x_test, y_test, 5000, 0.005)
 lr.call()
 loss, val_loss = lr.get_losses()
 
